c27d8874308bfe5e8e8b5b7b81bec59c37d49faa
intentionalfeeding <42819867+intentionalfeeding@users.noreply.github.com>
2018-10-19 02:09:34 -0400

    Update EfficientWordMarkov.java

--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -18,14 +18,18 @@ public class EfficientWordMarkov extends BaseWordMarkov{
 	public void setTraining(String text){
 		myWords = text.split(" ");
 		myMap.clear();
+		String word = "";
 		for (int i = 0; i<myWords.length-myOrder+1;i++){
 			WordGram temp = new WordGram(myWords,i,myOrder);
+			if(i+myOrder-1 != myWords.length-1){
+				word = myWords[i+myOrder];
+			}
 			if (!myMap.containsKey(temp)){
 				ArrayList<String> newAl = new ArrayList<String>();
 				if (i+myOrder >= myWords.length){
 					newAl.add(PSEUDO_EOS);
 				} else{
-					newAl.add(myWords[i+myOrder]);
+					newAl.add(word);
 				}
 				myMap.put(temp, newAl);
 			} else {
@@ -33,7 +37,7 @@ public class EfficientWordMarkov extends BaseWordMarkov{
 				if (i+myOrder >= myWords.length){
 					updateAl.add(PSEUDO_EOS);
 				} else{
-					updateAl.add(myWords[i+myOrder]);
+					updateAl.add(word);
 				}
 				myMap.put(temp, updateAl);
 			}
